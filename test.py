86b2f61b21e75350f1d375be036203ffd8be3e19
jamedrano <jamedrano@uvg.edu.gt>
2024-05-12 10:04:56 -0600

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,13 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as mt
 
+def preprocess(data):
+  data.columns = data.columns.str.strip()
+  for col in data.columns:
+    if data[col].dtype == 'O':
+      data[col] = data[col].str-strip()
+  
+
 st.set_page_config(page_title='Modelo Predictivo Resistencia a la Compresión CEMPRO', page_icon=None, layout="wide")
 
 tab1, tab2, tab3, tab4, tab5 = st.tabs(['Datos', 'Descripcion Datos', 'Graficos', 'Modelo', 'Descargar Datos'])
@@ -26,6 +33,8 @@ if uploaded_file is not None:
     data = pd.read_excel(uploaded_file,header=h,sheet_name=sh,engine='openpyxl')
     return data
   data = load_data(uploaded_file,sh,h)
+
+  data = preprocess(data)
   
   with tab1:
     st.write( '### 1. Datos Cargados ')
