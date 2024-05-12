76e4ca7bd31ba538f3ab241074e019ec1fa16010
jamedrano <jamedrano@uvg.edu.gt>
2024-05-12 08:53:42 -0600

    Update test.py

--- test.py
+++ test.py
@@ -26,26 +26,26 @@ if uploaded_file is not None:
     file_path = uploaded_file
 
    
-        try:
-            #User prompt to select sheet name in uploaded Excel
-            sh = st.sidebar.selectbox("*Which sheet name in the file should be read?*",pd.ExcelFile(file_path).sheet_names)
-            #User prompt to define row with column names if they aren't in the header row in the uploaded Excel
-            h = st.sidebar.number_input("*Which row contains the column names?*",0,100)
-        except:
-            st.info("File is not recognised as an Excel file")
-            sys.exit()
+    try:
+        #User prompt to select sheet name in uploaded Excel
+        sh = st.sidebar.selectbox("*Which sheet name in the file should be read?*",pd.ExcelFile(file_path).sheet_names)
+        #User prompt to define row with column names if they aren't in the header row in the uploaded Excel
+        h = st.sidebar.number_input("*Which row contains the column names?*",0,100)
+    except:
+        st.info("File is not recognised as an Excel file")
+        sys.exit()
        
     #Caching function to load data
     @st.cache_data(experimental_allow_widgets=True)
     def load_data(file_path,ft,sh,h):
         
        
-            try:
-                #Reading the excel file
-                data = pd.read_excel(file_path,header=h,sheet_name=sh,engine='openpyxl')
-            except:
-                st.infdo("File is not recognised as an Excel file.")
-                sys.exit()
+        try:
+            #Reading the excel file
+            data = pd.read_excel(file_path,header=h,sheet_name=sh,engine='openpyxl')
+        except:
+            st.infdo("File is not recognised as an Excel file.")
+            sys.exit()
          
         
         return data
